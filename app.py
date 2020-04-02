546a8e9cbf1408ef616c47ba2eee8365b3a0d8b4
andynavy23 <andynavy23@gmail.com>
2020-04-02 23:15:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ def upload_file():
         if file_to_upload:
             upload_result = upload(file_to_upload,
              resource_type="raw", 
-             folder = "/", 
-             public_id = "report_folder/" + 'test')
+             folder = "report_folder/", 
+             public_id = 'test')
             txt = '上傳成功！'
         else:
             txt = '請上傳檔案！'
