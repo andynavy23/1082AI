169bdf365de00bb2b55deeed496ae6d9c95d8137
andynavy23 <andynavy23@gmail.com>
2020-04-06 01:00:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -173,8 +173,7 @@ def handle_text_message(event):
             user_data = users_db.find({},{"userID": 1, "groupID": 1})
             user_match_output = userID in user_data
             group_match_output = groupID in user_data
-            print(user_match_output,group_match_output)
-            if user_match_output == None and group_match_output == None:
+            if user_match_output == False and group_match_output == False:
                 user_info = {"groupID": groupID,
                             "userID": userID,
                             "display_name": profile.display_name,
